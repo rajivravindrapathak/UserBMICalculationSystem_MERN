2d1e2b9d8e4c914e4b5216471bb656b657408284
Rajiv Ravindra pathak <70104100+rajivravindrapathak@users.noreply.github.com>
2023-01-12 23:54:17 +0530

    Add files via upload

--- backened/index.js
+++ backened/index.js
@@ -1,1 +1,34 @@
+const express = require('express')
+const {connection} = require("./config/db")
+const {UserModel} = require("./models/UserModel")
 
+const app = express()
+
+app.use(express.json())
+
+app.get((req, res) => {
+    res.send("hello")
+})
+
+app.post("/signup", async (req, res) => {
+    const {name, email, password} = req.body
+    // console.log(name, email, password)
+    const new_user = ({
+        name,
+        email,
+        password
+    })
+    await new_user.save()
+})
+
+app.listen(8000, async () => {
+
+    try {
+        await connection
+        console.log("connection to database success")
+    } catch (err) {
+        console.log("not connect to batabase")
+        console.log(err)
+    }
+    console.log("listning on port 8000")
+})
